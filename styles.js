5f9493c0532a427c99ecbdbfceb109d71b16724b
sturgilltyffani <sturgilltyffani@gmail.com>
2017-09-27 09:11:25 -0500

    inital commit

---
+++ styles.js
@@ -0,0 +1,46 @@
+var myName = "tyffani sturgill";
+if (myName == 'tyffani sturgill'); {
+    console.log("Name:" + " " + myName.toUpperCase());
+}
+var myCareer = "Full Stack Web Developer";
+if (myCareer == 'Full Stack Web Developer'); {
+    console.log("Career:" + " " + myCareer.toString());
+}
+var Descrip = "I like tacos";
+if (Descrip == 'I like tacos'); {
+    console.log("Description:" + Descrip.toString());
+}
+var myInt = ["video games", "twitch", "languages", "travel"];
+if (myInt == 'video games, twitch, languages, travel'); {
+    console.log("My Interests:");
+    console.log ("*" + " " + myInt [0]);
+    console.log ("*" + " " + myInt [1]);
+    console.log ("*" + " " + myInt [2]);
+    console.log ("*" + " " + myInt [3]);
+}
+var Exp = ["personal trainer at gold's gym", "develop fitness plans for clients", "consultation agent at geek squad", "resolve client issues", "patient care technician at uab hospital", "assisted the nurses with patient care"];
+if (Exp == "personal trainer at gold's gym", "developed fitness plans for clients", "consultation agent at geek squad", "resolved client issues", "patient care technician at uab hospital", "assisted the nurses with patient care"); {
+    console.log("My Previous Experience:");
+    console.log ("*" + " " + Exp [0]);
+    console.log ("-" + " " + Exp [1]);
+    console.log ("*" + " " + Exp [2]);
+    console.log ("-" + " " + Exp [3]);
+    console.log ("*" + " " + Exp [4]);
+    console.log ("-" + " " + Exp [5]);
+
+}
+var mySkills = ["html", "css", "fast learner"];
+if (Exp == "html", "css", "fast learner"); {
+    console.log("My Skills:");
+    console.log ("*" + " " + mySkills [0]);
+    console.log ("*" + " " + mySkills [1]);
+    console.log ("*" + " " + mySkills [2]);
+}
+var Symbol = "`````````````````````````";
+if (Symbol == "`````````````````````````"); {
+    console.log (Symbol);
+}
+var Ending = "P.S. One of those skills is not true";
+if (Ending == "P.S. One of those skills is not true") {
+    console.log (Ending);
+}
